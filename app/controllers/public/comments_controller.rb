486039e5165a73006d553aa1ae88f823f1bed46d
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-08-06 23:05:58 +0000

    [Start] 投稿機能スタート

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,16 @@
 class Public::CommentsController < ApplicationController
   def create
+    postimage = PostImage.find(params[:postimage_id])
+    comment = current_user.post_comments.new(comment_params)
+    comment.postimage_id = postimage.id
+    comment.save
+    redirect_to postimage_path(post_image)
   end
 
-  def destroy
+  private
+
+  def comment_params
+    params.require(:post_comment).permit(:comment)
   end
+
 end
--- app/controllers/public/postimages_controller.rb
+++ app/controllers/public/postimages_controller.rb
@@ -1,22 +1,58 @@
 class Public::PostimagesController < ApplicationController
   def new
+    @postimage = Postimage.new
   end
 
   def index
+    @postimages = Postimage.all
   end
 
   def create
+    @postimage = Postimage.new(book_params)
+    @postimage.user_id = current_user.id
+    if @postimage.save
+      redirect_to postimage_path(@postimage), notice:  "You have created book successfully."
+    else
+      @postimages = Postimage.all
+      render 'index'
+    end
   end
 
   def edit
+    @postimage = Postimage.new(book_params)
   end
 
   def show
+    @postimage = Postimage.find(params[:id])
+    @postimage_comment = Comment.new
   end
 
   def update
+    @postimage = Postimage.find(params[:id])
+    if @postimage.update(postimage_params)
+      redirect_to postimage_path(@postimage), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @postimage = Postimage.find(params[:id])
+    @postimage.destroy
+    redirect_to postimages_path
   end
+
+  private
+
+  def postimage_params
+    params.require(:postimage).permit(:title, :body, :image)
+  end
+
+  def is_matching_login_user
+    postimage = Postimage.find(params[:id])
+    if(postimage.user_id != current_user.id)
+      redirect_to postimages_path
+    end
+  end
+
 end
