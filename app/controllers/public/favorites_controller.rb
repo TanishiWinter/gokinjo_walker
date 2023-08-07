f7584b5639df1384d4c11a049f4d99e7b1aa517a
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-08-07 09:19:21 +0000

    [Update] いいね機能追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,17 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    post_image = PostImage.find(params[:post_image_id])
+    post_image = PostImage.find(params[:postimage_id])
     favorite = current_user.favorites.new(post_image_id: post_image.id)
     favorite.save
-    redirect_to postimage_path(post_image)
+    redirect_to postimages_path
   end
 
   def destroy
-    post_image = PostImage.find(params[:post_image_id])
+    post_image = PostImage.find(params[:postimage_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
-    redirect_to postimage_path(post_image)
+    redirect_to postimages_path
   end
 
 end
