c0e3bf0c84dbfaf24b7c72ab0497f4da623d5cae
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-08-08 07:42:47 +0000

    [Update] 検索機能にユーザー検索追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,8 +3,15 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
+    @range = params[:range]
     @word = params[:word]
-    @postimage_address = PostImage.where('address LIKE ?', "%#{@word}%")
+
+    if @range == "User"
+      @users = User.where('name LIKE ?', "%#{@word}%")
+    else
+      @postimage_address = PostImage.where('address LIKE ?', "%#{@word}%")
+    end
+
   end
 
 end
