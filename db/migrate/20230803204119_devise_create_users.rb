e2898ac1eb93d9a50e6b7ce53ebdaad6e5314489
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-08-20 05:38:07 +0000

    remove null false from introduction

--- db/migrate/20230803204119_devise_create_users.rb
+++ db/migrate/20230803204119_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ""
 
       t.string :name, null: false, default: ""
-      t.text :introduction, null: false, default: ""
+      t.text :introduction
       t.boolean :is_deleted, null: false, default: false
 
       ## Recoverable
