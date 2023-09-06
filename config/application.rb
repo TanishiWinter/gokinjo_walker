1cd5090a469b544e27a42ea847239fcd2cdb9161
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-09-06 05:42:09 +0000

    [Update] タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# タイムゾーンを東京に設定
+config.time_zone = 'Tokyo'
+
 module GokinjoWalker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
